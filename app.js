14b269338036bf9cc143a8b30083e3ac6ad0cec8
ghigbie <gehigbie@gmail.com>
2017-03-08 00:42:27 +0000

    added first route

--- app.js
+++ app.js
@@ -5,9 +5,23 @@ var bodyParser  = require("body-parser"),
     request     = require("request"),
     express     = require("express"),
     app         = express();
-    
+
+
+
 app.get("/", function(req, res){
-    res.send("<h1>Slash page!</h1>");
+    res.redirect("index");
+});
+
+app.get("/home", function(req, res){
+    res.redirect("index");
+});
+
+app.get("/index", function(req, res){
+    res.render("index"); 
+});
+
+app.get("*", function(req, res){
+    res.render("notfound"); 
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
