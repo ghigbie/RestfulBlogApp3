94bb13e56b6e8983b2187d06eade60747182d515
ghigbie <gehigbie@gmail.com>
2017-03-08 06:59:38 +0000

    added mongoose schema

--- app.js
+++ app.js
@@ -11,8 +11,18 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+//mongoose model config (title, image, body, created)
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now()}
+});
+//mongoose schema compiled into model
+var Blog = mongoose.model("Blog", blogSchema);
 
 
+//RESTful Routes
 app.get("/", function(req, res){
     res.redirect("index");
 });
@@ -30,5 +40,5 @@ app.get("*", function(req, res){
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("SEVER IS RUNNING!!!");
+    console.log("SERVER IS RUNNING!!!");
 });
