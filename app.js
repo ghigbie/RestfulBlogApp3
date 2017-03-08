aac88f70addd344e0dd74f410fb6d3d6fdd3a7f6
ghigbie <gehigbie@gmail.com>
2017-03-08 00:20:00 +0000

    configured require statements in app.js file

---
+++ app.js
@@ -0,0 +1,11 @@
+var bodyParser  = require("body-parser"),
+    sanitizer   = require("express-sanitizer"),
+    mongoose    = require("mongoose"),
+    override    = require("method-override"),
+    request     = require("request"),
+    express     = require("express"),
+    app         = express();
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    con
+});
